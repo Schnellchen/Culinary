f415dde08227dda6a2c63b51cc9b3501045d5642
Schnellchen <delf97@mail.ru>
2019-09-06 16:47:38 +0300

    Working on script

--- js/main.js
+++ js/main.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
     $(".content").click(function(){
         $("#navbarSupportedContent").collapse('hide');
     });
+    $(this).scroll(function () {
+        if ($(document).scrollTop() + $(window).height() > $("#cook2").offset().top + $("#cook2").height() * 0.75 && $(document).scrollTop() - $("#cook2").offset().top < $("#cook2").height()) {
+            $("#cook2").css("visibility","visible");
+            $("#cook3").css("visibility","visible");
+            $("#cook2").animate({left: 0}, 1500);
+            $("#cook3").animate({right: 0}, 1500);
+            $(".agitation__header").fadeIn(2000);
+            $(".agitation__list").fadeIn(2800);
+        }
+    });
 });
